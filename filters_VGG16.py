2bbd61e9e89f8b8ba42ea9fd7d28e49002e0dc81
andreanr <andrea.navarrete126@gmail.com>
2017-12-11 17:57:13 -0600

    comentarios

--- filters_VGG16.py
+++ filters_VGG16.py
@@ -29,7 +29,7 @@ def deprocess_image(x):
     x += 0.5
     x = np.clip(x, 0, 1)
 
-    # convert to RGB array
+    # convierte a RGB
     x *= 255
     if K.image_data_format() == 'channels_first':
         x = x.transpose((1, 2, 0))
@@ -38,7 +38,6 @@ def deprocess_image(x):
 
 
 def normalize(x):
-    # utility function to normalize a tensor by its L2 norm
     return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
 
 
@@ -54,43 +53,42 @@ def get_kept_filters(layer_dict,
         print('Processing filter %d' % filter_index)
         start_time = time.time()
 
-        # we build a loss function that maximizes the activation
-        # of the nth filter of the layer considered
+        # funcion de perdida que amximiza la activacion del filtro
         layer_output = layer_dict[layer_name].output
         if K.image_data_format() == 'channels_first':
             loss = K.mean(layer_output[:, filter_index, :, :])
         else:
             loss = K.mean(layer_output[:, :, :, filter_index])
 
-        # we compute the gradient of the input picture wrt this loss
+        # calcula el gradiente de la imagen inicial
         grads = K.gradients(loss, input_img)[0]
 
-        # normalization trick: we normalize the gradient
+        # normalizar el gradiente
         grads = normalize(grads)
 
-        # this function returns the loss and grads given the input picture
+        # regresa la perdida y el gradiente 
         iterate = K.function([input_img], [loss, grads])
 
-        # step size for gradient ascent
+        # tamaño de paso
         step = 1.
 
-        # we start from a gray image with some random noise
+        # una imagen gris con ruido aleatorio
         if K.image_data_format() == 'channels_first':
             input_img_data = np.random.random((1, 3, img_width, img_height))
         else:
             input_img_data = np.random.random((1, img_width, img_height, 3))
         input_img_data = (input_img_data - 0.5) * 20 + 128
 
-        # we run gradient ascent for 20 steps
+        # ascenso del grandiente en 20 pasos
         for i in range(20):
             loss_value, grads_value = iterate([input_img_data])
             input_img_data += grads_value * step
 
             if loss_value <= 0.:
-            # some filters get stuck to 0, we can skip them
+            # Eliminar filtros que se quedan en 0
                 break
 
-        # decode the resulting input image
+        # decodificar la imagen resultante
         if loss_value > 0:
             img = deprocess_image(input_img_data[0])
             kept_filters.append((img, loss_value))
@@ -106,7 +104,7 @@ def get_kept_filters(layer_dict,
     height = n * img_height + (n - 1) * margin
     stitched_filters = np.zeros((width, height, 3))
 
-    # fill the picture with our saved filters
+    # Guardar la imágen
     for i in range(n):
         for j in range(n):
             img, loss = kept_filters[i * n + j]
@@ -120,14 +118,13 @@ if __name__ == '__main__':
     img_width = 128
     img_height = 128
 
-    # build the VGG16 network with ImageNet weights
+    # VGG16 network con pesos de  ImageNet
     model = VGG16(weights='imagenet', include_top=False)
     print(model.summary())
 
-    # using ImageNet images (tensor)
+    # usando las imagenes ImageNet (tensores)
     input_img = model.input
 
-    # get the symbolic outputs of each "key" layer (we gave them unique names)
     layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
 
     layer_name = 'block5_conv3'
